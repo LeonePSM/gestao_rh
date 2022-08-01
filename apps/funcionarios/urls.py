20d4d96b2b88316b110bb3e15a299908738e7c8f
Leone <leonesilva.10@gmail.com>
2022-08-01 15:20:58 -0300

    Adicionando Crud Funcioários

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import home
+from .views import (
+    FuncionariosList,
+    FuncionariosEdit,
+    FuncionariosDelete,
+    FuncionariosNovos,
+)
 
 urlpatterns = [
-    path('', home),
+    path('', FuncionariosList.as_view(), name="list funcionarios"),
+    path('editar/<int:pk>', FuncionariosEdit.as_view(), name="update funcionarios"),
+    path('deletar/<int:pk>', FuncionariosDelete.as_view(), name="delete funcionarios"),
+    path('novo/', FuncionariosNovos.as_view(), name="create funcionarios"),
 ]
